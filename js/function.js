822988505a51d4737db80af9cbb93c2b3f5a4a29
Denys <denisparastyuk@gmail.com>
2023-06-13 23:32:05 +0300

    Нужно больше функций

--- js/function.js
+++ js/function.js
@@ -1,12 +1,14 @@
-//checkLengthFunction
-/*
+//function which will check if length is more than required length.
+
 function checkStringLength (stringToCheck, maxStringLength) {
   return (stringToCheck.length) <= maxStringLength;
-};
+}
 
-console.log(checkStringLength('Мама', 5));*/
+console.log(checkStringLength('Мама', 5));
 
-/*function checkStringPalindrom(stringToCheck) {
+//function which will check if string is a palidrome.
+
+function checkStringPalindrom(stringToCheck) {
   let normalString = stringToCheck.replaceAll(' ', '').toUpperCase();
   let emptyString = '';
   for (let i = normalString.length - 1; i >= 0; i--) {
@@ -15,16 +17,19 @@ console.log(checkStringLength('Мама', 5));*/
   return emptyString === normalString;
 }
 
-console.log(checkStringPalindrom('довод'));*/
+console.log(checkStringPalindrom('довод'));
+
+//function which will find and return numbers in string.
 
 function checkNumbersInString (stringToCheck) {
   let emptyString = '';
-  for (let i = 0; i <= stringToCheck.length; i ++) {
-    if (!Number.isNaN(parseInt(stringToCheck[i], 10))) {
-      emptyString += stringToCheck[i];
+  let newString = stringToCheck.toString();
+  for (let i = 0; i <= newString.length; i ++) {
+    if (!Number.isNaN(parseInt(newString[i], 10))) {
+      emptyString += newString[i];
     }
   }
-  return emptyString;
+  return emptyString.toString();
 }
 
-console.log(checkNumbersInString('год -1.5'));
+console.log(checkNumbersInString(-135.355));
