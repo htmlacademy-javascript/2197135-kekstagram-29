80407eaa3b5c4b4e4a83dff3be37b6370d971d6f
Denys <denisparastyuk@gmail.com>
2023-06-15 07:38:38 +0300

    Нужно больше функций

--- js/function.ts
+++ js/function.ts
@@ -1,35 +1,46 @@
 //function which will check if length is more than required length.
 
-function checkStringLength (stringToCheck, maxStringLength) {
-	return (stringToCheck.length) <= maxStringLength;
+const enum Default {
+	MAX_LENGTH = 140,
 }
+
+const checkStringLength = ({length}: {length: number}, maxStringLength: number = Default.MAX_LENGTH) => length <= maxStringLength;
+
 
 checkStringLength('Мама', 5);
 
 //function which will check if string is a palidrome.
 
-function checkStringPalindrom(stringToCheck) {
+const checkStringPalindrom = (stringToCheck: string) => {
 	const normalString = stringToCheck.replaceAll(' ', '').toUpperCase();
+
 	let emptyString = '';
+
 	for (let i = normalString.length - 1; i >= 0; i--) {
 		emptyString += normalString.at(i);
 	}
+
 	return emptyString === normalString;
-}
+};
 
 checkStringPalindrom('довод');
 
 //function which will find and return numbers in string.
 
-function checkNumbersInString (stringToCheck) {
+const checkNumbersInString = (stringToCheck: string | number) => {
 	let emptyString = '';
+
 	const newString = stringToCheck.toString();
+
 	for (let i = 0; i <= newString.length; i ++) {
 		if (!Number.isNaN(parseInt(newString[i], 10))) {
 			emptyString += newString[i];
 		}
 	}
+
 	return parseInt(emptyString, 10);
-}
+};
 
 checkNumbersInString('кефир -20.5');
+
+export {checkStringLength, checkStringPalindrom, checkNumbersInString};
